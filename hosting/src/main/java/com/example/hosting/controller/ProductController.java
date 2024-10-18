c2a60015588e6e56483323407a16b8baf7188f54
yc <do36421@naver.com>
2024-10-18 10:54:09 +0900

    product db에 있는걸 index에 뿌려줌

--- hosting/src/main/java/com/example/hosting/controller/ProductController.java
+++ hosting/src/main/java/com/example/hosting/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.example.hosting.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.hosting.domain.Product;
 import com.example.hosting.repository.ProductRepository;
 import com.example.hosting.service.S3Service;
+
 
 import lombok.RequiredArgsConstructor;
 
@@ -36,6 +40,16 @@ public class ProductController {
 		productRepository.save(product);
 		
 		return "redirect:/";
+	}
+	
+	//선생님은 그냥 MainController에서 작업함
+	@GetMapping({"","/"})
+	public String showProduct(Model model) {
+		
+		List<Product> productList = productRepository.findAll();
+		model.addAttribute("products", productList);
+		
+		return "index";
 	}
 	
 }
--- hosting/src/main/webapp/WEB-INF/hosting/index.jsp
+++ hosting/src/main/webapp/WEB-INF/hosting/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+ <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +10,17 @@
 <body>
 	<h1>index page</h1>
 	<a href="/product">상품등록</a>
+	
+	<div>
+		<c:forEach var ="item" items="${products}">
+			<figure>
+				<p><img width="200px" alt="상품이미지" src="${item.imgUrl}"></p>
+				<p>제목 : ${item.name}</p>
+				<figcaption>설명 : ${item.description}</figcaption>
+				<p>가격 : ${item.price}</p>
+			</figure>
+		</c:forEach>
+	</div>
 
 </body>
 </html>
